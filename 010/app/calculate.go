0b7bd6a52211709bb972dfd64ce9e0e32e025d4e
Nikolai Kozlov <ru.dev47@kgk-global.com>
2024-12-03 17:02:05 +0300

    v0_1_0

---
+++ 010/app/calculate.go
@@ -0,0 +1,51 @@
+package app
+
+import (
+	"math"
+)
+
+func GetLB(mx [][]int) int {
+	count := len(mx)
+	sum := 0
+
+	for i := 1; i < count; i++ {
+		min := math.MaxInt
+		for j := 1; j < count; j++ {
+			if mx[i][j] < min {
+				min = mx[i][j]
+			}
+		}
+		sum += min
+	}
+	return sum
+}
+
+func GetH(mx [][]int, rowName, colName int, s *Store) int {
+	rootMx := GetRootMatrix()
+	_ = rowName
+
+	count := len(mx)
+	var sum int
+
+	for i := 1; i < count; i++ {
+		min := math.MaxInt
+		for j := 1; j < count; j++ {
+			if mx[i][j] < min {
+				min = mx[i][j]
+			}
+		}
+		sum += min
+	}
+	// nodeID := s.Tree[s.CurrentNodeID].ParentID
+	nodeID := s.CurrentNodeID
+	for nodeID != 0 {
+		sum += rootMx[s.Tree[nodeID].Out][s.Tree[nodeID].In]
+		nodeID = s.Tree[nodeID].ParentID
+	}
+	if nodeID == 0 {
+		sum += rootMx[rowName][colName]
+	}
+	// sum = sum + s.AddParentsWeight(s.CurrentNodeID)
+
+	return sum
+}
